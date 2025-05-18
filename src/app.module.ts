b95bc596af7ec43e78e0a157dd4071b4004b12c7
Nathan <natethepcspecialist@gmail.com>
2025-05-18 13:54:08 -0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,11 @@ import { CompaniesModule } from './Company/Company.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '35.226.98.160',
-      port: 3306,
-      username: 'nate',
-      password: 'Nate1234',
-      database: 'accountant',
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || 'Nate1234',
+      database: process.env.DB_NAME || 'accountant',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
