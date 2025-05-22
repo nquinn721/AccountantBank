de5672e47e595eed236ac0a9f3326a59c86aa4ce
Nathan <natethepcspecialist@gmail.com>
2025-05-22 12:19:46 -0400

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,15 +8,28 @@ import { RakeModule } from './Rake/Rake.module';
 import { TipModule } from './Tip/Tip.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+// const dbConfig = {
+//    type: 'mysql' as const,
+//       host: 'localhost',
+//       port: 3306,
+//       username: 'admin',
+//       password: 'password',
+//       database: 'accountant',
+// }
+
+const dbConfig = {
+  type: 'mysql' as const,
+  host: 'projects/heroic-footing-460117-k8/global/networks/default',
+  port: 3306,
+  username: 'root',
+  password: 'Root1234',
+  database: 'accountant',
+};
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'admin',
-      password: 'password',
-      database: 'accountant',
+      ...dbConfig,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
